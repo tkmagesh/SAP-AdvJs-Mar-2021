82ea935009c419c5ced8d1bc75771c1642809f35
Magesh Kuppan <tkmagesh77@gmail.com>
2021-03-12 14:42:18 +0530

    Inheritance in JavaScript

--- constructor-functions/Employee.js
+++ constructor-functions/Employee.js
@@ -14,4 +14,22 @@ function Employee(id, name, salary){
     this.name = name;
     this.salary = salary;
     //this -> returned by default
+
+    this.display = function(){
+        console.log(this.id, this.name, this.salary, this.city);
+    };
 }
+
+/* OR */
+
+function Employee(id, name, salary){
+    //this -> new object
+    this.id = id;
+    this.name = name;
+    this.salary = salary;
+    //this -> returned by default
+}
+
+Employee.prototype.display = function(){
+    console.log(this.id, this.name, this.salary, this.city);
+};
--- constructor-functions/Spinner.js
+++ constructor-functions/Spinner.js
@@ -9,3 +9,16 @@ function Spinner(){
     }
     //this -> returned by default
 }
+
+
+function Spinner(){
+    //this -> new object
+    this.__counter__ = 0;
+    //this -> returned by default
+}
+Spinner.prototype.up = function(){
+    return ++this.__counter__;
+};
+Spinner.prototype.down = function(){
+    return --this.__counter__;
+}
